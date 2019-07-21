5f60b3519559654e977c41083f73f06421f050ef
divine <cdivine304@gmail.com>
2019-07-21 10:22:51 +0530

    updated

---
+++ precache-manifest.f1768014e94537bcccbc09f2494f4979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37adba503735a9ce0b2710cc1701a3a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a1d3d128f3d2fd947d0",
+    "url": "/static/css/main.f0f09214.chunk.css"
+  },
+  {
+    "revision": "4253f12bc66d74c7a2d4",
+    "url": "/static/js/2.0a0e436f.chunk.js"
+  },
+  {
+    "revision": "5a1d3d128f3d2fd947d0",
+    "url": "/static/js/main.3c1dcb41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
